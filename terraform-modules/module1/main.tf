7520d46a2b54751a9e5e894f8c3d577016203a3c
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 14:11:46 +0530

    s

--- terraform-modules/module1/main.tf
+++ terraform-modules/module1/main.tf
@@ -26,6 +26,17 @@ resource "aws_instance" "nginx" {
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.sg.id]
 
+    user_data = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y apache2
+      sudo systemctl status apache2
+      sudo systemctl start apache2
+      sudo chown -R $USER:$USER /var/www/html
+      sudo echo "<html><body><h1>Hello this is module-1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      EOF
+
+
     tags = {
         Name = "${var.component}-${var.env}" 
     }
