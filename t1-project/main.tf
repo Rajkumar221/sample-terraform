cc2e9627ab3003f21ae5e00b1c9c0063a74b336b
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 10:32:07 +0530

    s

--- t1-project/main.tf
+++ t1-project/main.tf
@@ -9,7 +9,7 @@
 # }
 
 resource "aws_instance" "test" {
-  for_each = { for index, comp in var.component : index => comp }
+  for_each = toset(var.component)
 
   ami           = "ami-03265a0778a880afb"
   instance_type = var.instance_type
