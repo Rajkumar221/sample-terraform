cf71fea8498b356610007ecc7d1b471105e30a46
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 11:02:44 +0530

    s

--- t1-project/main.tf
+++ t1-project/main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "test" {
  count = length(var.component)
  ami = "ami-03265a0778a880afb"
  instance_type = var.instance_type
- vpc_security_group_ids = aws_security_group.allow_all.id
+ vpc_security_group_ids = [aws_security_group.allow_all.id]
   
   tags = {
     Name = "${var.component[count.index]}-${var.env}"
