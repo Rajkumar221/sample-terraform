e9bddafbc65436718e4576c0dcb0dacd3ba30dea
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 10:27:05 +0530

    s

--- t1-project/main.tf
+++ t1-project/main.tf
@@ -1,10 +1,20 @@
+# resource "aws_instance" "test" {
+#  count = length(var.component)
+#  ami = "ami-03265a0778a880afb"
+#  instance_type = var.instance_type
+
+#   tags = {
+#     Name = "${var.component[count.index]}-${var.env}"
+#   }
+# }
+
 resource "aws_instance" "test" {
- count = length(var.component)
- ami = "ami-03265a0778a880afb"
- instance_type = var.instance_type
+  for_each = { for index, comp in var.component : index => comp }
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "${var.component[count.index]}-${var.env}"
+    Name = "${each.value}-${var.env}"
   }
 }
-
