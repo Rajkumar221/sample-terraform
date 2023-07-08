17ff3b2f1d48efc1a10d786214a6deaab7423eb1
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 11:01:11 +0530

    s

--- t1-project/outputs.tf
+++ t1-project/outputs.tf
@@ -1,7 +1,7 @@
-# output "public_ip_test" {
-#     value = aws_instance.test[*].public_ip
-# }
-
 output "public_ip_test" {
-  value = [for instance in aws_instance.test : instance.public_ip]
+    value = aws_instance.test[*].public_ip
 }
+
+# output "public_ip_test" {
+#   value = [for instance in aws_instance.test : instance.public_ip]
+# }
