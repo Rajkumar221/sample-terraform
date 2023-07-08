ad93a0993c2f34f5a32f3ebcf31fb85b677d9f71
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 15:27:06 +0530

    s

--- terraform-modules/module1/main.tf
+++ terraform-modules/module1/main.tf
@@ -3,10 +3,17 @@ resource "aws_security_group" "sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["115.97.103.44/32"]
   }
 
   egress {
