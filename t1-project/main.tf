0f0365115867b4a83ae7b2974b12b2ec317f4b94
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-08 11:01:14 +0530

    s

--- t1-project/main.tf
+++ t1-project/main.tf
@@ -1,20 +1,43 @@
-# resource "aws_instance" "test" {
-#  count = length(var.component)
-#  ami = "ami-03265a0778a880afb"
-#  instance_type = var.instance_type
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
 
-#   tags = {
-#     Name = "${var.component[count.index]}-${var.env}"
-#   }
-# }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_instance" "test" {
-  for_each = toset(var.component)
-
-  ami           = "ami-03265a0778a880afb"
-  instance_type = var.instance_type
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "${each.value}-${var.env}"
+    Name = "allow_all"
   }
 }
+
+resource "aws_instance" "test" {
+ count = length(var.component)
+ ami = "ami-03265a0778a880afb"
+ instance_type = var.instance_type
+ vpc_security_group_ids = aws_security_group.allow_all.id
+  
+  tags = {
+    Name = "${var.component[count.index]}-${var.env}"
+  }
+}
+
+# resource "aws_instance" "test" {
+#   for_each = toset(var.component)
+
+#   ami           = "ami-03265a0778a880afb"
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "${each.value}-${var.env}"
+#   }
+# }
