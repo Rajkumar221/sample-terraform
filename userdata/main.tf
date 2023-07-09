59b46480d83f2103eca74468bb2538d59161ab6a
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-09 08:36:42 +0530

    s

--- userdata/main.tf
+++ userdata/main.tf
@@ -26,17 +26,33 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello, World!" > /tmp/greeting.txt
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     echo "Hello, World!" > /tmp/greeting.txt
+#     yum update -y
+#     yum install -y httpd
+#     systemctl start httpd
+#     systemctl enable httpd
+#   EOF
 
 
   tags = {
     Name = "example"
   }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/Rajkumar221/roboshop-ansible.git main.yml -e role_name=frontend"
+    ]
+  }
+
 }
