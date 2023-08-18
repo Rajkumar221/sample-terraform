b6af6951a2504c4cd50f4720f8835ea0eea5b1bf
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-18 11:14:54 +0530

    s

--- terraform-modules/module3/userdata.sh
+++ terraform-modules/module3/userdata.sh
@@ -6,8 +6,8 @@ yum update -y
 # Install Nginx
 yum install -y nginx
 
-# Start Nginx service
-systemctl start nginx
-
 # Enable Nginx to start on system boot
 systemctl enable nginx
+
+# Start Nginx service
+systemctl start nginx
