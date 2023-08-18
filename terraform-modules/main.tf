a512788a53a1480550703db1bb54e23fc31288e7
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-18 10:42:47 +0530

    ss

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -1,11 +1,17 @@
+# module "nginx" {
+#   source = ".//module1"
+
+#   ami = var.ami
+# }
+
+# module "appache" {
+#   source = ".//module2"
+
+#   ami = var.ami
+# }
+
 module "nginx" {
-  source = ".//module1"
-
-  ami = var.ami
-}
-
-module "appache" {
-  source = ".//module2"
+  source = ".//module3"
 
   ami = var.ami
 }
