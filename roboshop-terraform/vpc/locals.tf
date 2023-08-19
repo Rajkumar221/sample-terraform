7a3fb7fb5a98603cca96d3c50bd9f2405cdfa588
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-19 14:13:04 +0530

    s

--- roboshop-terraform/vpc/locals.tf
+++ roboshop-terraform/vpc/locals.tf
@@ -1,8 +1,8 @@
 locals {
-all_route_table_ids = [
-  module.subnets.app.route_table_ids,
-  module.subnets.web.route_table_ids,
-  module.subnets.db.route_table_ids,
-  module.subnets.public.route_table_ids
-]
+  all_route_table_ids = [
+    module.subnets["app"].route_table_ids,
+    module.subnets["web"].route_table_ids,
+    module.subnets["db"].route_table_ids,
+    module.subnets["public"].route_table_ids
+  ]
 }
