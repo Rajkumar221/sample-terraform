7f215ba8821af610dfb7649c81ec1d5d682dab4b
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-19 13:39:33 +0530

    s

--- roboshop-terraform/vpc/main.tf
+++ roboshop-terraform/vpc/main.tf
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_route" gw {
-  route_table_id            = module.subnets["public"].subnet.ids
+  route_table_id            = module.subnets["public"].route_table.ids
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
   
--- roboshop-terraform/vpc/subnets/main.tf
+++ roboshop-terraform/vpc/subnets/main.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
   count = length(var.cidr_blocks)
   vpc_id     = var.vpc_id
-  cidr_block = element(var.cidr_blocks, count.index)
+  cidr_block = var.cidr_blocks[count.index]
   availability_zone = element(var.az, count.index)
   
   tags = {
