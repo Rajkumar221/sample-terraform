d62d12fb981e98848f04689a81d11b42efd1b26f
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-19 14:22:40 +0530

    s

--- roboshop-terraform/vpc/subnets/outputs.tf
+++ roboshop-terraform/vpc/subnets/outputs.tf
@@ -2,6 +2,22 @@ output "subnet_ids" {
   value = aws_subnet.main.*.id
 }
 
-output "route_table_ids" {
-  value = aws_route_table.example.id
+
+output "app_route_table_ids" {
+  value = module.subnets.app.route_table_ids
 }
+
+output "web_route_table_ids" {
+  value = module.subnets.web.route_table_ids
+}
+
+output "db_route_table_ids" {
+  value = module.subnets.db.route_table_ids
+}
+
+output "public_route_table_ids" {
+  value = module.subnets.public.route_table_ids
+}
+
+
+
