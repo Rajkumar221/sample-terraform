60c09d3340716c4df8fbed9d6efa81c47081a67b
Rajkumar <rajkumarpatel52247@gmail.com>
2023-08-19 13:17:41 +0530

    s

--- roboshop-terraform/vpc/subnets/variables.tf
+++ roboshop-terraform/vpc/subnets/variables.tf
@@ -1,10 +1,10 @@
 variable "cidr_block" {
-    default = {
-        web = { cidr_block = ["10.0.1.0/24", "10.0.2.0/24"] }
-        app = { cidrblock = ["10.0.3.0/24", "10.0.4.0/24"] }
-        db = { cidr_block = ["10.0.5.0/24", "10.0.6.0/24"] }
-        public = { cidr_block = ["10.0.7.0/24", "10.0.8.0/24"] }
-    }
+  default = {
+    web = { cidr_block = ["10.0.1.0/24", "10.0.2.0/24"] }
+    app = { cidrblock = ["10.0.3.0/24", "10.0.4.0/24"] }
+    db = { cidr_block = ["10.0.5.0/24", "10.0.6.0/24"] }
+    public = { cidr_block = ["10.0.7.0/24", "10.0.8.0/24"] }
+  }
 }
 
 variable "az" {
